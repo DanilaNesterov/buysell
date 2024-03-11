a72d495df78610f4bc2420e2a7e6015077a298df
DanilaNesterov <104511422+DanilaNesterov@users.noreply.github.com>
2024-03-12 02:03:20 +0700

    Add files via upload

---
+++ buysell/src/main/java/com/example/buysell/controllers/ProductController.java
@@ -0,0 +1,52 @@
+package com.example.buysell.controllers;
+
+
+import com.example.buysell.services.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.buysell.models.Product;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public String products(Model model) {
+        model.addAttribute("products", productService.listProduct());
+        return "products";
+
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model){
+
+        model.addAttribute("product", productService.getProductById(id));
+        return "product-info";
+
+    }
+
+
+    @PostMapping("/product/create")
+    public String createProduct(Product product){
+        productService.saveProduct(product);
+        return "redirect:/";
+
+    }
+
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/";
+
+    }
+
+}
