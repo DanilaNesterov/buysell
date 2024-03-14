5bf1a5df4b19ca3f20476bfb9ece1a10d51980b6
DanilaNesterov <104511422+DanilaNesterov@users.noreply.github.com>
2024-03-15 00:37:35 +0700

    Add files via upload

--- buysell/src/main/java/com/example/buysell/models/Product.java
+++ buysell/src/main/java/com/example/buysell/models/Product.java
@@ -1,7 +1,7 @@
 package com.example.buysell.models;
 
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
